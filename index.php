dff07e4bc535dc51164d30e7c4c5c527d2c26c7f
Moustapha Sabir <moustaphasabirmoussa61@gmail.com>
2024-05-22 11:35:34 +0200

    index

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="styles/reset.css">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="styles/boulangerie.css">
+</head>
+<body>
+    <header>
+        <div class="contenairlien">
+            <nav><a href="index.php">Accueil</a></nav>
+            <nav><a href="pages/boulangerie.php">Boulangerie</a></nav>
+            <nav><a href="pages/chocolaterie.php">Chocolaterie</a></nav>
+            <nav><a href="pages/patisserie.php">Patisserie</a></nav>
+            <nav><a href="pages/formulaire.php">Contact</a></nav>
+        </div>
+    </header>
+    <main>
+        <img src="images/imagesAcceuil/fondd'ecran.png" alt="pain">
+    </main>
+    <footer>
+        <div class="container">
+            <div class="adresse">
+                <h3>Horaires :</h3>
+                <p><strong>Du Lundi au Vendredi :</strong></p>
+                <p>7h30 à 13h00 et 14h à 19h30</p>
+                <p><strong>Samedi :</strong></p>
+                <p>7h30 à 13h00 et 14h à 19h30</p>
+                <p><strong>Dimanche et jours fériés :</strong></p>
+                <p>7h00 à 13h00</p>
+            </div>
+            <div class="logo">
+                <img src="images/imagesAcceuil/logo.jpg" alt="logo de l'entreprise">
+            </div>
+            <div class="boulangerie">
+                <div class="poitou">
+                    <h3>Boulangerie-Patisserie :</h3>
+                    <p>Adresse : 6 Av. Galilée, Bd Louis Braille</p>
+                    <p><strong>86360 Chasseneuil-du-Poitou</strong></p>
+                    <p><strong>Tel : 0605954351</strong></p>
+                    <br>
+                    <h3>CONTACT :</h3>
+                    <a href="pages/formulaire.html" class="contact">Envoyez-nous un message !</a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
